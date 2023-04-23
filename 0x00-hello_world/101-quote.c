39588b3bcc1cbaa2c1fd0b04cf60ab14f61cf95a
viviankemboi <jeptooviviank@gmail.com>
2023-04-23 13:21:03 -0700

    hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,25 +1,15 @@
 #include <stdio.h>
-
-#include <unistd.h>
-
+#include <unistdh.h>
 
 /**
-
-* main - Entry point
-
-*
-
-* Return: Always 1 (Success)
-
-*/
+ * main - entry point program that prints a line to the standard error
+ *
+ * Return: 1 Indicates success
+ */
 
 int main(void)
 
 {
-	write(2,
-
-        "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-
-        return (1);
-
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
 }
